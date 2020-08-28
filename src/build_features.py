a1173847a21198386058ed08ff20f1cf78b22fae
Maciej Garbacz <2203950g@student.gla.ac.uk>
2020-08-28 13:44:59 +0200

    Most of the feature expansion and selection completed, advanced selection to be completed

--- src/build_features.py
+++ src/build_features.py
@@ -2,42 +2,38 @@ import pandas as pd
 from src import helpers as h
 
 
-
-#A function to expand the existing columns of non-ordinal categorical features
-#into one-hot encoded features and perfom bucketization on those feature that permit it
-
+# A function to expand the existing columns of non-ordinal categorical features
+# into one-hot encoded features and perfom bucketization on those feature that permit it.
+# Returns the expanded, cleaned DataFrame object containing the attributes and the target variable
 def feature_expansion(dataset):
+    # Read in the data into a DataFrame
+    df = pd.read_csv("../data/" + dataset + ".csv", sep=";")
 
-    #Read in the data into a DataFrame
-    df = pd.read_csv("../data/" + dataset +".csv", sep = ";")
-
-    #Record the target variable
+    # Record the target variable
     target = df["Survived"]
 
-    #Introduce two new variables - total family size and a binary flag for solo travellers
+    # Introduce two new variables - total family size and a binary flag for solo travellers
     df["FamilySize"] = df["SibSp"] + df["Parch"] + 1
     df["IsAlone"] = 0
     df.loc[df["FamilySize"] == 1, "IsAlone"] = 1
 
-    #Expand the Embarked variable into one-hot encoded features
+    # Expand the Embarked variable into one-hot encoded features
     embarked_onehot_features = pd.get_dummies(df['Embarked'])
     df = pd.concat([df, embarked_onehot_features], axis=1)
 
-    #Expand the Sex variable into one-hot encoded features
+    # Expand the Sex variable into one-hot encoded features
     sex_onehot_features = pd.get_dummies(df['Sex'])
     df = pd.concat([df, sex_onehot_features], axis=1)
 
-    #Perform bucketization on the Age attribute
-    df['Age_cat'] = df['Age'].apply(h.catregorize_age)
+    # Perform bucketization on the Age attribute
+    df['Age_cat'] = df['Age'].apply(h.categorize_age)
 
-    #Drop attributes which won't be used for the training
-    del(df["Name"])
-    del(df["Ticket"])
-    del(df["Cabin"])
-    del(df["PassengerId"])
+    # Perform bucketization on the Fare attribute
+    df['Fare_cat'] = df['Fare'].apply(h.categorize_fare)
 
+    # Drop attributes which won't be used for the training
+    h.delete_unnecessary_attributes(df)
 
-    print(df.columns.values.tolist())
+    return df, target
 
-
-feature_expansion("train")
+feature_expansion('train')
--- src/helpers.py
+++ src/helpers.py
@@ -1,22 +1,41 @@
-#Function to assign age of a passenger to 1 of 10 available categories
-def catregorize_age(age):
-    if age < 10:
-        return 0
-    if age < 18:
+#Function to assign age of a passenger to 1 of 6 available categories
+def categorize_age(age):
+    if age <= 19:
         return 1
-    if age < 22:
+    if age <= 26:
         return 2
-    if age < 25:
+    if age <= 33:
         return 3
-    if age < 28:
+    if age <= 45:
         return 4
-    if age < 30:
+    if age <= 81:
         return 5
-    if age < 35:
-        return 6
-    if age < 42:
-        return 7
-    if age < 55:
-        return 8
-    if age < 85:
-        return 9
+    else:
+        return 0
+
+#Function to assign fare of a passenger to 1 of 5 available categories
+def categorize_fare(fare):
+    if fare <= 7.9:
+        return 1
+    if fare <= 13.0:
+        return 2
+    if fare <= 26.3:
+        return 3
+    if fare <= 73.5:
+        return 4
+    else:
+        return 5
+
+#Clean the DataFrame object to contain only attributes used during training
+def delete_unnecessary_attributes(df):
+    del (df["Name"])
+    del (df["Ticket"])
+    del (df["Cabin"])
+    del (df["PassengerId"])
+    del (df["Embarked"])
+    del (df["Sex"])
+    del (df["Age"])
+    del (df["SibSp"])
+    del (df["Parch"])
+    del (df["Fare"])
+    del (df["Survived"])
